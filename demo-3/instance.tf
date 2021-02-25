7f36c4e692b2b77fbbc023a17f310f90645c8c45
cjwbrandon <cjwbrandon@gmail.com>
2021-02-25 21:13:10 +0800

    [ADD] Outputting example

--- demo-3/instance.tf
+++ demo-3/instance.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "example" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
+
+  // Runs command on local machine -> only works for linux or macs
+  // Can use this file for other uses such as in an Ansible playbook
   provisioner "local-exec" {
     command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
   }
